27e47b04852ffe9bf14b75f005ab84bb33ad5663
GuyH <guyh@GuyHs-MacBook-Pro.local>
2024-03-19 10:52:03 +0200

    add docker creds

--- buildScripts/jenkins/dsl/release.groovy
+++ buildScripts/jenkins/dsl/release.groovy
@@ -50,6 +50,8 @@ job(JOB_NAME) {
         sshAgent('kgithub-build-jenkins-microcosm-key')
         credentialsBinding {
             usernamePassword('MICROSERVICES_ARTIFACTORY_USER', 'MICROSERVICES_ARTIFACTORY_PASSWORD', 'jcasc_deployer-microcosm')
+            string('DOCKERHUB_TOKEN_ENV', 'DOCKERHUB_TOKEN')
+            string('DOCKER_ARTIFACTORY_TOKEN_ENV', 'DOCKER_ARTIFACTORY_TOKEN')
         }
     }
 
